6a71d21491425fb9d9c443a49d22f45a63f2a19c
Sarmad rafique <sarmadrafique040@gmail.com>
2024-01-30 21:55:30 +0500

    Sign in form component

---
+++ app/(auth)/components/sign-in-form.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+export const SignInForm = () => {
+  return <div>Sign in Form</div>;
+};
