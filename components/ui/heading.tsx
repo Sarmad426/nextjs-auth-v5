57b6f362326e994f9facae2b6894728dc487ab29
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-03 08:24:58 +0500

    Made the description text optional

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,13 +1,13 @@
 type HeadingProps = {
   title: string;
-  description: string;
+  description?: string;
 };
 
 export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div className="my-2">
-      <h2 className="text-2xl font-semibold">{title}</h2>
-      <p className="my-2 text-gray-600">{description}</p>
+      <h2 className="text-3xl font-semibold">{title}</h2>
+      {description && <p className="my-2 text-gray-600">{description}</p>}
     </div>
   );
 };
