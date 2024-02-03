04e52041df65e8090fb1e7400a3c2e0137ca01bc
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-03 08:11:55 +0500

    Login form

--- app/(auth)/components/sign-in-form.tsx
+++ app/(auth)/components/sign-in-form.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
   Form,
   FormControl,
@@ -9,6 +12,66 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
+import { LoginSchema, typeLoginSchema } from "@/schema/form-schema";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export const SignInForm = () => {
-  return <div>Sign in Form</div>;
+  const form = useForm<typeLoginSchema>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: typeLoginSchema) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="my-2">
+          <FormField
+            name="email"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    name="email"
+                    placeholder="sarmad@email.com"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="my-2">
+          <FormField
+            name="password"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex justify-center">
+          <Button className="w-1/2" type="submit">
+            Login
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
 };
