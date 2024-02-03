5638c35fae99bf6a10eea9e84ba434c0a9238e43
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-03 07:27:03 +0500

    Added doc-string and type for login schema

--- schema/form-schema.ts
+++ schema/form-schema.ts
@@ -1,9 +1,17 @@
 import { z } from 'zod'
 
+/**
+ * Login and Register Form Schemas
+ * Types of Login and Register Form Schemas
+ */
+
 export const LoginSchema = z.object({
     email: z.string().email({ message: "invalid Email!" }),
     password: z.string().min(1, { message: "invalid Password!" })
 })
+
+export type typeLoginSchema = z.infer<typeof LoginSchema>
+
 
 export const RegisterSchema = z.object({
     name: z.string().min(2, { message: "Name of 2 characters is required!" }),
