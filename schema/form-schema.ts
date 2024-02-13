dbdc379e859e183ccb8003099a2cf83618e22034
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-13 19:09:39 +0500

    updated form error messages

--- schema/form-schema.ts
+++ schema/form-schema.ts
@@ -6,8 +6,8 @@ import { z } from 'zod'
  */
 
 export const LoginSchema = z.object({
-    email: z.string().email({ message: "⚠️invalid Email" }),
-    password: z.string().min(1, { message: "⚠️invalid Password" })
+    email: z.string().email({ message: "invalid Email⚠️" }),
+    password: z.string().min(1, { message: "invalid Password⚠️" })
 })
 
 export type typeLoginSchema = z.infer<typeof LoginSchema>
@@ -15,8 +15,8 @@ export type typeLoginSchema = z.infer<typeof LoginSchema>
 
 export const RegisterSchema = z.object({
     name: z.string().min(2, { message: "Name of 2 characters is required!" }),
-    email: z.string().email({ message: "⚠️invalid Email" }),
-    password: z.string().min(1, { message: "Password of characters is required!" })
+    email: z.string().email({ message: "invalid Email⚠️" }),
+    password: z.string().min(1, { message: "Password of 6 characters is required!" })
 })
 
 
