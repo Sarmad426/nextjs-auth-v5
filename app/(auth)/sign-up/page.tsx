3c97d8449c1a183c68f9b3f8bd9c24eab493f907
Sarmad rafique <sarmadrafique040@gmail.com>
2024-02-13 19:15:53 +0500

    Added form components

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,15 +1,24 @@
 import { Metadata } from "next";
-import { SignInForm } from "../components/sign-in-form";
+import { SignUpForm } from "../components/sign-up-form";
+import { Heading } from "@/components/ui/heading";
+import { CardWrapper } from "../components/card-wrapper";
 
 export const metadata: Metadata = {
-  title: "Sign in",
-  description: "Sign in to AUTH",
+  title: "Sign Up",
+  description: "Sign up to AUTH",
 };
 
 export default function SignIn() {
   return (
-    <div>
-      <SignInForm />
+    <div className="flex items-center justify-center mb-4 flex-col w-full space-y-6">
+      <Heading title="🔐AUTH" />
+      <CardWrapper
+        title="Sign Up"
+        footerContent="Already have an account?"
+        url="sign-in"
+      >
+        <SignUpForm />
+      </CardWrapper>
     </div>
   );
 }
